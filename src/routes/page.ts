1aa5defdff08b5c3ab4a747ec937d374c8ab889c
FillinGood <fargust97@gmail.com>
2022-10-10 02:07:23 +0700

    page access permissions

--- src/routes/page.ts
+++ src/routes/page.ts
@@ -1,6 +1,15 @@
 import express from 'express';
 import fs from 'fs';
 import rootpath from '../rootpath';
+import User from '../wrappers/users';
+
+async function checkAccess(page: string, user?: User) {
+  if (user) {
+    return (await user.checkAccess(page)) || (await user.group.checkAccess(page));
+  } else {
+    return User.checkGuestAccess(page);
+  }
+}
 
 const pageRoute = express
   .Router()
@@ -12,6 +21,10 @@ const pageRoute = express
       return;
     }
     req.context.page = req.params.page;
+    const access = await checkAccess(req.context.page, req.user);
+    if (!access) {
+      req.context.page = '403';
+    }
     res.render('index', req.context);
   })
 
